c8e63c045ac4828c7a1ac827936d4b5965ecf516
Elena <lesikr@ya.ru>
2022-02-26 22:08:36 +0300

    break

--- Lesson2_metotharray/Program.cs
+++ Lesson2_metotharray/Program.cs
@@ -1,5 +1,5 @@
-﻿//ищем индекс заданного числа массива
-int [] array = {1,2,33,94,55,66,27,8};
+﻿//ищем первый индекс для заданного числа массива
+int [] array = {1,2,33,27,94,55,66,27,8};
 
 int n = array.Length;
 int find = 27;
@@ -11,6 +11,7 @@ while(index<n)
     if(array[index] == find)
     {
         Console.WriteLine(index);
+        break;
     }
     index++; //index=index+1
 }
