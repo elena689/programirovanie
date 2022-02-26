cbd94a98dcb5864ac97135277be7d412a3eefdf3
Elena <lesikr@ya.ru>
2022-02-26 22:04:32 +0300

    indexarray

---
+++ Lesson2_metotharray/Program.cs
@@ -0,0 +1,16 @@
+﻿//ищем индекс заданного числа массива
+int [] array = {1,2,33,94,55,66,27,8};
+
+int n = array.Length;
+int find = 27;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++; //index=index+1
+}
