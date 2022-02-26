875131c8b6238b74af0913190a5b4adb0a6e9076
Elena <lesikr@ya.ru>
2022-02-26 17:40:43 +0300

    dogs

---
+++ Lesson1_dogs/Program.cs
@@ -0,0 +1,28 @@
+﻿int FFS = 1;
+int SFS = 2;
+int dogSpeed = 5;
+int distance = 10000;
+int time = 0;
+int friend = 2;
+//Console.SetCursorPosition(xb, yb);
+//Console.WriteLine("+");
+
+int count = 0;
+
+while(distance > 10)
+{
+        if(friend == 1)
+    {
+        time = distance/(FFS + dogSpeed);
+        friend = 2;
+    }
+        else
+    {
+        time = distance/(SFS + dogSpeed);
+        friend = 1;
+    }
+    
+    distance = distance + (FFS + SFS) * time;
+    count = count + 1;
+}
+Console.WriteLine("Собака пробежит " +count+ " раз(а)");
