9566355f2d78c34b36a79d9bd101af6c77d027b3
Elena <lesikr@ya.ru>
2022-03-20 10:18:13 +0300

    замена символов в тексте

---
+++ Lesson3_zamena/Program.cs
@@ -0,0 +1,29 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить на черточки. Все строчные "к" заменить на заглавные. Все заглавные "С", заменить на строчные.
+
+string text = "С утра падал снег."
+            + "Медвежонок сидел на пеньке." 
+            + "Он поднял голову. "
+            + "Малыш считал упавшие на носу снежинки." 
+            + "Снежинки падали пушистые и белые.";
+
+// string s = "qwerty"
+//             012
+// s[3]//r
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i<length; i++)
+    {
+        if (text[i]==oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '_');
+newText = Replace(newText, 'к', 'К');
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
